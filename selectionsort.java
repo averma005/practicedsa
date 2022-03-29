ece9d75aa5525ab61dd0207a086c083d4595943a
averma005 <galvatrona.k.s@gmail.com>
2022-03-29 13:43:37 +0530

    completed selectionsort

--- selectionsort.java
+++ selectionsort.java
@@ -5,16 +5,17 @@ class sorting{
         }
     }
     public static void main(String args[]){
-        int small =0;
+        // int small =0;
         int arr[] = {7,8,3,1,2};
         for(int i=0;i<arr.length-1;i++){
-            for(int j=0; j<arr.length-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    small=arr[j+1];
-                }else{
-                    small=arr[j];
-                }
+            int small = i;
+            for(int j=i+1; j<arr.length;j++){
+                if(arr[small] >arr[j])
+                small =j;
             }
+            int temp = arr[small];
+            arr[small]= arr[i];
+            arr[i]=temp;
         }
         printer(arr);
     }
